b032d5133328defba6ba3a609bcdc51208aa005e
Matej Melic <matej@matejmeglic.com>
2022-05-17 00:42:29 +0200

    First deploy to Heroku

--- server/index.js
+++ server/index.js
@@ -113,6 +113,8 @@ const app = express();
 
 var jsonParser = bodyParser.json();
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.post('/api', jsonParser, async function (req, res) {
     counter = 0
     apiArray = []
@@ -143,6 +145,10 @@ app.post('/open', jsonParser, async function (req, res) {
     console.log(assetPath)
     await open(assetPath)
 })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
